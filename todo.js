b539ee5eed31500335229e6dc9e9088f44cae348
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-05-05 05:46:52 +0700

    minus filter, order by date and completed

--- todo.js
+++ todo.js
@@ -32,6 +32,14 @@ switch (argv[2]) {
     uncompleted(argv[3]);
     break;
 
+  case "tag":
+    tag(argv);
+    break;
+
+  case "filter":
+    filter(argv[3]);
+    break;
+
   default:
   help()
 }
@@ -43,18 +51,21 @@ function help() {
   console.log("Masukkan (delete task_id) untuk menghapus task");
   console.log("Masukkan (completed task_id) untuk menandai task completed");
   console.log("Masukkan (uncompleted task_id) untuk menhapus tanda task completed");
+  console.log("Masukkan (tag task_id input_tag_1 input_tag_2 input_tag_...) untuk menambahkan tag pada task");
 }
 
 function list() {
-  console.log("id | task                  | completed?");
+  console.log("id|task|completed?|created at|tag");
   let check = "";
   for (let i = 0 ; i < data.length ; i++ ){
+    let created_time = data[i].created_at
+    let created_time_utc = created_time
     if (data[i].completed == true){
       check = "[X]"
     } else {
       check = "[ ]"
     }
-    console.log(`${data[i].id}  | ${data[i].task}                 | ${check}`);
+    console.log(`${data[i].id}|${data[i].task}|${check}|${created_time_utc}|${data[i].tag}`);
   }
 }
 
@@ -70,6 +81,8 @@ function add(task_name) {
   }
   tmp["task"] = task_name;
   tmp["completed"] = false;
+  tmp["created_at"] = new Date();
+  tmp["tag"] = [];
   tmp_str = tmp["task"];
   data.push(tmp);
   fs.writeFile('data.json', JSON.stringify(data) , (err) => {
@@ -124,3 +137,31 @@ function uncompleted(task_id) {
   console.log('The file has been saved!');
   });
 }
+
+function tag(argv) {
+  let index_id = "";
+  let tmp_tag = [];
+  for (let o = 0; o < data.length; o++){
+    if (data[o].id == argv[3]){
+      index_id = o;
+    }
+  }
+
+  for (let l = 4; l < argv.length;l++){
+    tmp_tag.push(argv[l]);
+  }
+
+  data[index_id].tag = tmp_tag;
+
+  fs.writeFile('data.json', JSON.stringify(data) , (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+  });
+}
+
+// filter(){
+//   let indexs_filter = [];
+//   for (){
+//
+//   }
+// }
