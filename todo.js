51a25f9ae5182d3adf9fe78252c6d67fbd228fff
Sidik Hidayatullah <sidik.riders@gmail.com>
2017-05-06 16:08:41 +0700

    akhirnya release 3 uhuk

--- todo.js
+++ todo.js
@@ -33,6 +33,10 @@ class Model{
       this.control.uncomplete();
     } else if (mas[2] === 'delete') {
       this.control.delete();
+    } else if (mas[2] === 'tag') {
+      this.control.addTag();
+    } else if (mas[2] === 'filter' || mas[2] === 'filter:') {
+      this.view.filter;
     }
   }
 
@@ -48,8 +52,11 @@ class View{
       console.log('\x1B[2J')
       console.log(`\n\n--------------Selamat datang di applikasi TODOS---------------\n\nUntuk memulai silahkan ucapkan 'Hai Todo!!', dengan nada ceria\n\n--------------------------------------------------------------`)
       console.log('\nBerikut daftar command untuk menjalan aplikasi ini:')
-      console.log('\n1. node todo.js list\n=====> untuk melihat daftar rencana mu.\n\n2. node todo.js add <new task>:<tags>\n=====> untuk menambah daftar rencana mu\n\n3. node todo.js complete <task number>\n=====> untuk merubah status rencana mu menjadi selesai.')
+      console.log('\n1. node todo.js list\n=====> untuk melihat daftar rencana mu.\n\n2. node todo.js add <new task>:<tags>\n=====> untuk menambah daftar rencana mu.\n\n3. node todo.js complete <task number>\n=====> untuk merubah status rencana mu menjadi selesai.')
       console.log('\n4. node todo.js uncomplete <task number>\n=====> untuk merubah status rencamu menjadi belum selesai.\n\n5. node todo.js delete <task number>\n=====> untuk menghapus daftar rencanamu.')
+      console.log('\n6. node todo.js list:outstanding\n=====> untuk melihat daftar rencanamu yang belum selesai (optional desc|asc).\n\n7. node todo.js list:completed\n=====> untuk melihat daftar rencanamu yang sudah selesai (optional desc|asc).')
+      console.log('\n8. node todo.js tag <task number> <tags>\n=====> untuk merubah tags pada daftar acaramu.\n\n9. node todo.js filter: <tag>\n=====> untuk menampilkan daftar berdasarkan tag nya. (maksimal masih satu tag aja per search)')
+      console.log('\n10. node todo.js help\n=====> untuk menampilkan daftar perintah dalam mengoperasikan applikasi ini.')
   }
 
   get list() {
@@ -73,18 +80,20 @@ class View{
   get outstanding() {
     console.log('\x1B[2J')
     if (this.inputView[3] === 'desc') {
-      var alert = 'Berikut daftar acaramu, di urutkan dari yang ter BARU ke yang ter LAMA:'
+      var alert = 'Berikut daftar acaramu yang belum selesai, di urutkan dari yang ter BARU ke yang ter LAMA:'
       console.log(alert);
-      console.log('_______________________________________________________________________\n')
+      console.log('_________________________________________________________________________________________\n')
       if (this.dataTask.length < 1) {
         console.log('<kosong>')
       }
-      let urut = this.dataTask;
+      let urut = this.dataTask.filter(x => {if ( x.sudah == false ) {
+        return x;
+      }});
       urut.sort((a,b) => {return b.createdDate - a.createdDate})
       let listing = urut.map( x => {
         let stat = '[ ]'
         if (x.sudah == true) {stat = '[X]'}
-        return `${stat} ${x.task}     <tags: ${x.tags.join(', ')}>`;
+        return `${stat} ${x.task}  <tags: ${x.tags.join(', ')}>`;
       });
       for (let i=0;i<urut.length;i++) {
         let nomor = i+1
@@ -94,32 +103,82 @@ class View{
       urut.sort((a,b) => {return a.createdDate - b.createdDate })
 
     } else {
-      var alert = 'Berikut daftar acaramu, di urutkan dari yang ter LAMA ke yang ter BARU:'
+      var alert = 'Berikut daftar acaramu yang belum selesai, di urutkan dari yang ter LAMA ke yang ter BARU:'
       console.log(alert);
-      console.log('_______________________________________________________________________\n')
+      console.log('__________________________________________________________________________________________\n')
       if (this.dataTask.length < 1) {
         console.log('<kosong>')
       }
-      let urut = this.dataTask;
+      let urut = this.dataTask.filter(x => {if ( x.sudah == false ) {
+        return x;
+      }});
+      let listing = urut.map( x => {
+        let stat = '[ ]'
+        if (x.sudah == true) {stat = '[X]'}
+        return `${stat} ${x.task}  <tags: ${x.tags.join(', ')}>`;
+      });
       for (let i=0;i<urut.length;i++) {
         let nomor = i+1
-        console.log(`${nomor}. ${urut[i].task}, tags: ${urut[i].tags.join(' ')}`)
+        console.log(`${nomor}. ${listing[i]}`)
       }
       console.log('\n\n                             ~Gimana bisa kalem kalau rasanya dalem')
     }
   }
 
   get completed() {
-    console.log('\x1B[2J');
-    var komplited = this.dataTask.filter(x => x.sudah)
-    var alert = 'Berikut daftar acaramu yang sudah selesai:'
-    console.log(alert);
-    if (komplited.length < 1) {
-      console.log('belom ada yang kelar coyy tugasnyaaa')
-    }
-    console.log(komplited)
+    if (this.inputView[3] === 'desc') {
+      console.log('\x1B[2J');
+      var komplited = this.dataTask.filter(x => x.sudah);
+      if (komplited.length < 1) {
+        console.log('belom ada yang kelar coyy tugasnyaaa')
+      } else {
+        komplited.sort((a,b) => {return b.dateCompleted - a.dateCompleted});
+        var alert = 'Berikut daftar acaramu yang sudah selesai,\ndiurutkan berdasarkan waktu diselesaikannya dari terBARU ke terLAMA:'
+        console.log(alert);
+        console.log('____________________________________________________________________\n')
+        var list = komplited.map( x => {
+          let waktuSelesai = new Date(x.dateCompleted);
+          return `${x.task}, selesai pada tanggal: ${waktuSelesai}`;
+        })
+        for (let i=0;i<list.length;i++) {
+          console.log(`${i+1}. ${list[i]}`)
+        }
+        komplited.sort((a,b) => {return a.dateCompleted - b.dateCompleted});
+      }
 
+    } else {
+      console.log('\x1B[2J');
+      var komplited = this.dataTask.filter(x => x.sudah)
+      var alert = 'Berikut daftar acaramu yang sudah selesai,\ndiurutkan berdasarkan waktu diselesaikannya dari terLAMA ke terBARU:'
+      console.log(alert);
+      console.log('____________________________________________________________________\n')
+      if (komplited.length < 1) {
+        console.log('belom ada yang kelar coyy tugasnyaaa')
+      }
+      var list = komplited.map( x => {
+        let waktuSelesai = new Date(x.dateCompleted);
+        return `${x.task}, selesai pada tanggal: ${waktuSelesai}`;
+      })
+      for (let i=0;i<list.length;i++) {
+        console.log(`${i+1}. ${list[i]}`)
+      }
+    }
   }
+
+  get filter() {
+    console.log('\x1B[2J');
+    var tagFilter = this.inputView[3];
+    if (tagFilter == undefined) {
+      console.log('hm? salah ketik ya? cek `node todo.js help` untuk bantuan')
+    } else {
+      var taskFiltered = this.dataTask.filter(x => x.tags.indexOf(tagFilter) !== -1);
+      console.log(`Menampilkan daftar acara berdasarkan tag: ${tagFilter}\n`);
+      for (let i=0;i<taskFiltered.length;i++) {
+        console.log(`${i+1}. ${taskFiltered[i].task}        tags: <${taskFiltered[i].tags.join(', ')}>`)
+      }
+    }
+  }
+
 }
 
 class Control{
@@ -166,6 +225,8 @@ class Control{
     } else if (this.inputControl[3] <= this.dataTask.length && this.inputControl[3] > 0) {
       console.log('\x1B[2J')
       this.dataTask[(this.inputControl[3])-1].sudah = true;
+      var tanggalKelar = (new Date()).getTime();
+      this.dataTask[(this.inputControl[3])-1].dateCompleted = tanggalKelar;
       var dataString = JSON.stringify(this.dataTask);
       fs.writeFileSync('data.json', dataString, 'utf-8');
       console.log(this.dataTask[this.inputControl[3]-1].task.toUpperCase() + ', sudah berubah status menjadi complete!!\n\n========== Ayo segera selesaikan daftar rencanamu!! ==========\n\n\           jangan perkeruh situasi kecuali situasu')
@@ -178,11 +239,12 @@ class Control{
   uncomplete() {
     if (this.inputControl.length <= 3) {
       console.log('\x1B[2J')
-      console.log('Hmmm , sepertinya command mu salah, coba ketik seperti ini:\n\nnode todo.js complete < nomor task yang kamu ingin ubah statusnya >\n\nPaham??, ayo coba lagi!!')
+      console.log('Hmmm , sepertinya command mu salah, coba ketik seperti ini:\n\nnode todo.js uncomplete < nomor task yang kamu ingin ubah statusnya >\n\nPaham??, ayo coba lagi!!')
 
     } else if (this.inputControl[3] <= this.dataTask.length && this.inputControl[3] > 0) {
       console.log('\x1B[2J')
       this.dataTask[(this.inputControl[3])-1].sudah = false;
+      this.dataTask[(this.inputControl[3])-1].dateCompleted = null;
       var dataString = JSON.stringify(this.dataTask);
       fs.writeFileSync('data.json', dataString, 'utf-8');
       console.log(this.dataTask[this.inputControl[3]-1].task.toUpperCase() + ', sudah berubah status menjadi uncomplete!!\n\n========== Ayo segera selesaikan daftar rencanamu!! ==========\n\ngimana bisa kalem kalau rasanya dalem ~~')
@@ -210,6 +272,51 @@ class Control{
       console.log('Hmmm , sepertinya command mu salah, coba ketik seperti ini:\n\nnode todo.js complete < nomor task yang kamu ingin hapus >\n\nPaham??, ayo coba lagi!!')
     }
   }
+
+  addTag() {
+    if(this.inputControl.length<=3){
+      console.log('\x1B[2J')
+      console.log('Hmmm? , sepertinya command mu salah, coba ketik seperti ini:\n\nnode todo.js tag <nomor task yang ingin kamu rubah tag nya> <tag yang ingin kamu tambahkan/hilangkan>\n\nPaham??, ayo coba lagi!!')
+
+    } else {
+      var tagTambahan = this.inputControl.slice(3, this.inputControl.length);
+      console.log('\x1B[2J');
+      var nomorTask = tagTambahan[0];
+      var tagBaru = tagTambahan.slice(1, tagTambahan.length);
+      var tambahTag = [];
+      //proses penghapusan tags
+      var indexTagYangdiHapus = [];
+      for (let i=0; i<tagBaru.length;i++) {
+        let indexHapus = this.dataTask[nomorTask-1].tags.indexOf(tagBaru[i]);
+        if (indexHapus !== -1) {
+          indexTagYangdiHapus.push(indexHapus);
+        } else {
+          tambahTag.push(tagBaru[i]);
+        };
+      }
+      if (indexTagYangdiHapus.length > 0) {
+        var tagYangdiHapus = indexTagYangdiHapus.map(x=>this.dataTask[nomorTask-1].tags[x]);
+        console.log(`${tagYangdiHapus.join(', ')} berhasil dihapus dari tag task nomor ${nomorTask}\n`);
+        for (let i=0; i<indexTagYangdiHapus.length; i++){
+          this.dataTask[nomorTask-1].tags.splice(indexTagYangdiHapus[i],1);
+        }
+        var dataString = JSON.stringify(this.dataTask);
+        fs.writeFileSync('data.json', dataString, 'utf-8');
+      }
+      //proses penambahan tags
+      for (let i=0; i<tambahTag.length;i++) {
+        if (this.dataTask[nomorTask-1].tags.indexOf(tambahTag[i]) == -1) {
+          this.dataTask[nomorTask-1].tags.push(tambahTag[i])
+        }
+      }
+      if (tambahTag.length > 0) {
+        console.log(`${tambahTag.join(', ')} berhasil ditambahkan ke tag task nomor ${nomorTask}`);
+      }
+      var dataString = JSON.stringify(this.dataTask);
+      fs.writeFileSync('data.json', dataString, 'utf-8');
+    }
+  }
+
 }
 
 
